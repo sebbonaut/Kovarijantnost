9b27657f80f151cd0f8445661fd3bbbc58ce2c8e
sebas <sebas@DESKTOP-S92RB8H>
2023-12-11 22:22:05 +0100

    Add project files.

---
+++ Kovarijantnost/Osoba.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kovarijantnost
+{
+    internal interface IDohvati<out T>
+    {
+        public T? IDohvati();
+    }
+    internal interface IDodaj<in T>
+    {
+        public void Dodaj(T novi);
+    }
+    internal class Grupa<T> : IDohvati<T>, IDodaj<T>
+    {
+        public List<T> clanovi = [];
+
+        public void Dodaj(T novi)
+            => clanovi.Add(novi);
+
+        public T? IDohvati()
+        {
+            if (clanovi is null || clanovi.Count == 0)
+                return default;
+            T temp = clanovi[0];
+            clanovi.RemoveAt(0);
+            return temp;
+        }
+    }
+    internal class Osoba
+    {
+        public string id = "";
+        public virtual Osoba Klon()
+            => new() { id = id };
+    }
+    internal class Zaposlenik : Osoba
+    {
+        public decimal placa = 0;
+        public override Zaposlenik Klon()
+            => new Zaposlenik() { id = id, placa = placa };
+    }
+    internal class Student : Osoba
+    {
+        public double prosjek = 0;
+    }
+}
---
+++ Kovarijantnost/Program.cs
@@ -0,0 +1,26 @@
+﻿using Kovarijantnost;
+
+Zaposlenik o1 = new()
+{
+    id = "123"
+};
+Zaposlenik o2 = o1.Klon();
+o1.id = "234";
+Console.WriteLine(o2.id);
+
+Student o3 = new() { id = "123" };
+Osoba _ = Zaposli(o3);
+
+Grupa<Student> analiza = new()
+{
+    clanovi = [o3]
+};
+IDohvati<Osoba> osobe = analiza;
+IDodaj<Osoba> osobe2 = new Grupa<Osoba>();
+IDodaj<Student> studenti = osobe2;
+studenti.Dodaj(new Student { id = "123" });
+
+static Zaposlenik Zaposli(Osoba a)
+{
+    return new Zaposlenik() { id = a.id, placa = 1000 };
+}
